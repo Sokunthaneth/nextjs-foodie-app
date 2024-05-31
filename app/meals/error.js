1bed02fd50fd7a0ece6bb01ac21e399da240705d
sokunthaneth <sokunthaneth.chhoy@nbc.gov.kh>
2024-05-31 10:00:41 +0700

    init project

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage({ error }) {
+  return (
+    <main className="not-found">
+      <h1>An error occured!</h1>
+      <p>Something has gone wrong at our side!</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found!</h1>
+      <p>Sorry, the page you are looking for cannot be found.</p>
+    </main>
+  );
+}
